0fe65c5296e36bc4bbf47e9b0db2ccd8cbb07a9f
LisaT2016 <may4992@gmail.com>
2021-10-22 19:42:31 -0400

    Update contact_me.js

--- docs/assets/mail/contact_me.js
+++ docs/assets/mail/contact_me.js
@@ -20,6 +20,7 @@ $(function () {
             }
             $this = $("#sendMessageButton");
             $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
+            var action = $(this).attr("action");
             $.ajax({
                 url: "", // /assets/mail/contact_me.php
                 type: action,
